64a2975f29e377322d2475282cfc1a6c6cd99b87
Piotrek Dąbrowski <floubadour@gmail.com>
2016-02-16 13:13:42 +0100

    environment config (from hackathon-starter)

--- config/config.js
+++ config/config.js
@@ -7,30 +7,30 @@ module.exports = {
 
   // OAuth 2.0
   facebook: {
-    clientId: '657854390977827',
-    secret: 'FACEBOOK_SECRET',
+    clientId: '754220301289665',
+    secret: '41860e58c256a3d7ad8267d3c1939a4a',
     url: '/api/auth/facebook'
   },
   google: {
-    clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com',
-    secret: 'GOOGLE_SECRET',
+    clientId: '828110519058.apps.googleusercontent.com',
+    secret: 'JdZsIaWhUFIchmC1a_IZzOHb',
     url: '/api/auth/google'
   },
   github: {
-    clientId: '0ba2600b1dbdb756688b',
-    secret: 'GITHUB_SECRET',
+    clientId: 'cb448b1d4f0c743a1e36',
+    secret: '815aa4606f476444691c5f1c16b9c70da6714dc6',
     url: '/api/auth/github'
   },
 
   linkedin: {
-    clientId: '77cw786yignpzj',
-    secret: 'LINKEDIN_SECRET',
+    clientId: '77chexmowru601',
+    secret: 'szdC8lN2s2SuMSy8',
     url: '/api/auth/linkedin'
   },
 
   instagram: {
-    clientId: '799d1f8ea0e44ac8b70e7f18fcacedd1',
-    secret: 'INSTAGRAM_SECRET',
+    clientId: '9f5c39ab236a48e0aec354acb77eee9b',
+    secret: '5920619aafe842128673e793a1c40028',
     url: '/api/auth/instagram'
   },
 
@@ -59,16 +59,16 @@ module.exports = {
   },
 
   foursquare: {
-    clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
-    secret: 'FOURSQUARE_SECRET',
+    clientId: '2STROLSFBMZLAHG3IBA141EM2HGRF0IRIBB4KXMOGA2EH3JG',
+    secret: 'UAABFAWTIHIUFBL0PDC3TDMSXJF2GTGWLD3BES1QHXKAIYQB',
     authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate',
     url: '/api/auth/foursquare'
   },
 
   // OAuth 1.0
   twitter: {
-    key: 'YOUR_TWITTER_CONSUMER_KEY',
-    secret: 'TWITTER_SECRET',
+    key: '6NNBDyJ2TavL407A3lWxPFKBI',
+    secret: 'ZHaYyK3DQCqv49Z9ofsYdqiUgeoICyh6uoBgFfu7OeYC7wTQKa',
     url: '/api/auth/twitter'
   }
 };
